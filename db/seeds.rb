7db6f9fb886a09a6eeaf5223933204832ec914a6
scarlettperry <si.perry15@gmail.com>
2018-10-16 18:26:55 -0400

    uncommented seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
-Event.destroy_all
-UserEvent.destroy_all
+# Event.destroy_all
+# UserEvent.destroy_all
 
 u1 = User.create(name: "Scarlett", username: "scarpy", password: "123")
-e1 = Event.create(exhibition_name: "New Art City")
+# e1 = Event.create(exhibition_name: "New Art City")
 
-UserEvent.create(user_id: u1.id, event_id: e1.id)
+# UserEvent.create(user_id: u1.id, event_id: e1.id)
